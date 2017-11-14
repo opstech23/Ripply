95b0e30b6b0d6e00cc3518e586f7eb63879f8667
opstech23 <bbody8215@gmail.com>
2017-11-14 14:31:20 +0800

    add disable onclick to voting button once voted

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :projects do
+  	member do
+  		post 'upvote'
+  	end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'projects#index'
 end
